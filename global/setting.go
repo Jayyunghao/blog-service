a5037e2fa5e3a6a66ef8fe70185169fc50867b1b
索渝东 <suoyudong@jd.com>
2021-07-02 20:33:09 +0800

    until validate

---
+++ global/setting.go
@@ -0,0 +1,13 @@
+package global
+
+import (
+	"Practice/go-programming-tour-book/blog-service/pkg/logger"
+	"Practice/go-programming-tour-book/blog-service/pkg/setting"
+)
+
+var (
+	ServerSetting		*setting.ServerSettinsS
+	AppSetting 			*setting.AppSettingS
+	DatabaseSetting 	*setting.DatabaseSettingS
+	Logger              *logger.Logger
+)
---
+++ main.go
@@ -0,0 +1,87 @@
+package main
+
+import (
+	_ "Practice/go-programming-tour-book/blog-service/docs"
+	"Practice/go-programming-tour-book/blog-service/global"
+	"Practice/go-programming-tour-book/blog-service/internal/model"
+	"Practice/go-programming-tour-book/blog-service/internal/routers"
+	"Practice/go-programming-tour-book/blog-service/pkg/logger"
+	setting2 "Practice/go-programming-tour-book/blog-service/pkg/setting"
+	"fmt"
+	"github.com/gin-gonic/gin"
+	"gopkg.in/natefinch/lumberjack.v2"
+	"log"
+	"net/http"
+	"time"
+)
+
+// @title 博客系统
+// @version 1.0
+// @description Go 语言编程之旅：一起用 Go 做项目
+// @termsOfService https://github.com/go-programming-tour-book
+func main() {
+	gin.SetMode(global.ServerSetting.RunMode)
+	router := routers.NewRouter()
+	s := &http.Server{
+		Addr:      ":" + global.ServerSetting.HttpPort,
+		Handler:     router,
+		ReadTimeout: global.ServerSetting.ReadTimeout,
+		WriteTimeout: global.ServerSetting.WriteTimeout,
+		MaxHeaderBytes: 1<<20,
+	}
+	global.Logger.Infof("%s: go-programming-tour-book/%s", "eddycjy", "blog_service")
+	s.ListenAndServe()
+}
+
+func init() {
+	err := setupSetting()
+	if err != nil {
+		log.Fatalf("init.setupSetting err :%v",err)
+	}
+	err = setupLogger()
+	if err != nil {
+		log.Fatalf("init.setupLogger err :%v",err)
+	}
+}
+
+func setupSetting() error {
+	setting, err := setting2.NewSetting()
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Server",&global.ServerSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("App",&global.AppSetting)
+	if err != nil {
+		return err
+	}
+	err = setting.ReadSection("Database",&global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	fmt.Sprintf("\nserverSetting : %v\nappSetting : %v\n Database: %v\n",global.ServerSetting,global.AppSetting,global.DatabaseSetting)
+	global.ServerSetting.ReadTimeout *= time.Second
+	global.ServerSetting.WriteTimeout *= time.Second
+	return nil
+}
+
+func setupLogger() error {
+	global.Logger = logger.NewLogger(&lumberjack.Logger{
+		Filename: global.AppSetting.LogSavePath + "/" + global.AppSetting.LogFileName + global.AppSetting.LogFileExt,
+		MaxSize:  600,
+		MaxAge:   10,
+		LocalTime: true,
+	},"",log.LstdFlags).WithCaller(2)
+	return nil
+}
+
+func setupDBEngine() error {
+	var err error
+	global.DBEngine, err = model.NewDBEngine(global.DatabaseSetting)
+	if err != nil {
+		return err
+	}
+	return nil
+}
